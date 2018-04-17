e5eb82df4c5d3e70e6486cedab55c572dbcda9fa
Mark <floydmate@gmail.com>
2018-04-17 03:58:46 -0500

    avances en tabla hash

--- tabla-hash/tabhash.cpp
+++ tabla-hash/tabhash.cpp
@@ -8,23 +8,65 @@
 class Elemento{
     public:
         int val;
-        Lista *exp;
+        //Lista *exp;
+        int t_exp;
+        int *exp;
+        Elemento(){ val = 0; t_exp = 0; exp = NULL;}
+};
+class TablaHash{
+    public:
+        int insertar(int);
+        int buscar(int);
+    private:
+        Elemento tabla[MEM];
+        int n_digitos(int);
+        int mod_hash(int);
+        int cm_hash(int);
+        int exp_ins(Elemento*,int);
+        int exp_busc(Elemento*,int);
 };
 
 using namespace std;
-Elemento tabla[MEM];
-int fhash(int x){
+
+int TablaHash::mod_hash(int x){
     return x%MEM;
 }
-int insertar(int val){
-    int hash = fhash(val);
+int TablaHash::n_digitos(int i){
+    int dec = 10;
+    int cont = 0;
+    while(i>0){
+        i = i - i%dec;
+        dec *=10;
+        cont++;
+    }
+    return cont;
+}
+int TablaHash::cm_hash(int val){
+    int cont = n_digitos(val);
+    int ret = 0;
+    if(cont <= 2){
+        return val%MEM;
+    }
+    char *num = (char*)calloc(cont,sizeof(char));
+    sprintf(num,"%d",val);
+    num[cont/2+1] = '\0';
+    ret = atoi(num+cont/2-1);
+    free(num);
+    return ret;
+}
+int TablaHash::exp_ins(Elemento *em,int val){
+    em->t_exp++;
+    em->exp = (int*)realloc(em->exp,(em->t_exp)*sizeof(int));
+    em->exp[em->t_exp-1] = val;
+}
+int TablaHash::insertar(int val){
+    int hash = cm_hash(val);
     //Comprobar si la posición en la tabla hash esta ocupada
     //en caso de que el valor sea 0 puede reescribir :s
     Elemento *tempElem = tabla+hash;
     if(tempElem->val){
         if(!tempElem->exp)
-            tempElem->exp = new Lista();
-        tempElem->exp->insertar(val);
+            exp_ins(tempElem,val);
         return val;
     }else{
         tempElem->val = val;
@@ -32,35 +74,35 @@ int insertar(int val){
     }
     return 0;
 }
-int buscar(int val){
-    int hash = fhash(val);
+int TablaHash::exp_busc(Elemento *em,int val){
+    int p = 0;
+    //p = bb(val,0,em->t_exp-1,em->exp);
+    //return (**(tab+p)==h)?tab[p][1]:-1;
+}
+int TablaHash::buscar(int val){
+    int hash = cm_hash(val);
     Elemento *tempElem = tabla+hash;
     if(tempElem->val != val){
-        return (tempElem->exp)?tempElem->exp->buscar(val):0;
+        return (tempElem->exp)?exp_busc(tempElem,val):0;
     }else{
         return tempElem->val;
     }
 }
+int bb(int h,int n,int q,int *m){
+    if(n==q){
+        return *(m+n);
+    }else{
+        int p = (n+q)/2;
+        if(*(m+p)<h)
+            return bb(h,p+1,q,m);
+        else
+            return bb(h,n,p,m);
+    }
+}
 int main(){
-    Lista l;
-    int j = 0;
-    int i = 100;
-    int q = 0;
-    int nums[i];
-    //int nums[] = {67,175,25,0,-4,35,67,175,25};
-    //int i = sizeof(nums)/sizeof(nums[0]);
-    FILE *f = fopen("numbers","w+");
-    srand(time(NULL));
-    for(;j < i;j++){
-        nums[j] = rand()%1000;
-        fprintf(f,"%d\n",nums[j]);
-    }
-    for(j = 0;j < i;j++){
-        insertar(nums[j]);
-    }
-    fclose(f);
-    for(;;){
-        cin >> q;
-        printf("buscar:%d\n",buscar(q));
-    }
+    TablaHash tb;
+    int nums[] = {3,4,5,7,9};
+    int num = 4;
+    int out = bb(num,0,4,nums);
+    printf("buscar:%d\n",out);
 }
