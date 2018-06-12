1790efb20c1548129dae0a8d779510fa17818339
Mark <floydmate@gmail.com>
2018-06-11 23:42:48 -0500

    nuevos métodos

--- arbol/arb-bin.h
+++ arbol/arb-bin.h
@@ -3,12 +3,14 @@
 #include "nodoabin.h"
 class ArbolBinario{
     void ins(Carrera*,NABin*);
+    GpoCarreras *buscar(int idGpo,NABin *raiz);
     public:
     NABin *raiz;
     void insertar(Carrera*);
     void inOrden(NABin *n);
     void preOrden(NABin *n);
     void postOrden(NABin *n);
+    GpoCarreras *buscarGrupo(int);
     void niveles();
     ArbolBinario(){
         raiz = NULL;
