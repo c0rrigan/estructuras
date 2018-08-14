7b5af6fd5fad96f4de82f496fc026df824337db3
mark <floydmate@gmail.com>
2018-08-13 23:56:47 -0500

    ultima version de programa que calcula cardinalidades

--- cardinalidades.cpp
+++ cardinalidades.cpp
@@ -3,21 +3,30 @@
 #include <string>
 
 int main(int argc,char* argv[]){
-    unsigned int n = 3;
-    if(argc == 2){
-        n = std::stoi(argv[1]);
-    }
     unsigned int char_count = 0;
     char c;
     std::streampos str_pos = 0;
+    unsigned int n = 3;
+    std::string seed = "0\n1\n";
+    std::string filename = "alph";
+    if(argc == 1){
+        std::cout << "sigma: 3\n,file out: alph" << std::endl;
+    }
+    if(argc > 1){
+        n = std::stoi(argv[1]);
+        std::cout << "sigma:" << argv[1] << std::endl;
+    }
+    if(argc > 2){
+        filename = argv[2];
+        std::cout << "file out:" << argv[2] << std::endl;
+    }
     //create seed file
-    std::string seed = "00\n01\n10\n11\n";
-    std::ofstream seed_file ("alph",std::ios::trunc);
+    std::ofstream seed_file (filename,std::ios::trunc);
     seed_file << seed.c_str();
     seed_file.close();
     //reopen file as fstream
-    std::fstream file ("alph",std::fstream::in | std::fstream::out | std::fstream::app);
-    while(char_count < n){
+    std::fstream file (filename,std::fstream::in | std::fstream::out | std::fstream::app);
+    while(true){
         char_count = 0;
         std::string buffer;
         file.seekg(str_pos);
@@ -34,6 +43,10 @@ int main(int argc,char* argv[]){
         file << buffer.c_str();
     }
     file.close();
+    //add 'e' at the beggining of the file
+    std::fstream final_file (filename,std::fstream::out|std::fstream::in);
+    final_file << 'e';
+    final_file.close();
     return 0;
 }
 
